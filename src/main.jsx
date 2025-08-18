a4f8150915f909597a6e6186eefed1fd468187eb
bikram107 <149646889+bikram107@users.noreply.github.com>
2025-08-18 20:00:27 +1000

    updated router setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppContextProvider } from "./AppContext.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import "animate.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <Router>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </Router>
   </StrictMode>
 );
